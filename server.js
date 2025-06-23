5e003b9828caf8a3b14a1eace452517e45ca06e3
macksyn <155353088+macksyn@users.noreply.github.com>
2025-06-23 15:36:24 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,28 +1,29 @@
+// ----------------------
+// 📄 server.js
+// ----------------------
+
 const express = require("express");
 const qrcode = require("qrcode");
 const { init } = require("./bot");
-
 const app = express();
+
 let sock;
 
 app.get("/", (req, res) => {
-  if (sock && sock.authState?.creds?.noiseKey) {
-    res.send("Bot already connected 👍");
-  } else {
-    res.send(`<img src="/qr.png"/><p>Scan to connect</p>`);
-  }
+  res.send(`<h1>Lussh MD is running!</h1><p>Visit <a href='/qr.png'>/qr.png</a> to scan the QR code.</p>`);
 });
 
 app.get("/qr.png", async (req, res) => {
-  if (!sock) {
-    sock = await init();
-  }
+  if (!sock) sock = await init();
+
   sock.ev.once("connection.update", async (u) => {
     if (u.qr) {
       const img = await qrcode.toDataURL(u.qr);
       const base64 = img.split(",")[1];
       res.setHeader("Content-Type", "image/png");
       res.send(Buffer.from(base64, "base64"));
+    } else {
+      res.status(204).send("No QR code available");
     }
   });
 });
